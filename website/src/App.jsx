3a2d7b105bb592eb8d477be7a0ba84ba3ddfd19a
Gabe-Alonso <gabixtreme363@outlook.com>
2025-12-04 18:36:23 -0800

    Added Navigable Header

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,7 +1,8 @@
 import {useEffect, useState} from 'react'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, Link, useNavigate} from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 import './App.css'
 import {HomePage} from "./HomePage.jsx";
 import {ProjectsPage} from "./ProjectsPage.jsx";
@@ -21,20 +22,47 @@ function App() {
     }, []);
 
     return (
-        <div>
-            <h1>App exists</h1>
-            <BrowserRouter>
+
+        <BrowserRouter>
+            <HeaderBar/>
+            <div className="page-container">
                 <Routes>
-                    <Route path="/" element={<HomePage />}></Route>
-                    <Route path="/projects" element={<ProjectsPage />}></Route>
-                    <Route path="/music" element={<MusicPage />}></Route>
-                    <Route path="/resume" element={<ResumePage />}></Route>
-                    <Route path="/blog" element={<BlogPage />}></Route>
-                    <Route path="/contact" element={<ContactPage />}></Route>
+                    <Route path="/" element={<HomePage/>}></Route>
+                    <Route path="/projects" element={<ProjectsPage/>}></Route>
+                    <Route path="/music" element={<MusicPage/>}></Route>
+                    <Route path="/resume" element={<ResumePage/>}></Route>
+                    <Route path="/blog" element={<BlogPage/>}></Route>
+                    <Route path="/contact" element={<ContactPage/>}></Route>
                 </Routes>
-            </BrowserRouter>
-        </div>
-  )
+            </div>
+        </BrowserRouter>
+
+    )
+}
+
+function HeaderBar() {
+    return (
+        <header className="header">
+            <div className="container">
+                <header className="topbar">
+                    <div className="name">Gabriel da Costa Alonso</div>
+
+                    <nav className="socials">
+                        <a href="https://www.linkedin.com/in/gabe-alonso/" target="_blank"><FaLinkedin size={24}/></a>
+                        <a href="https://github.com/Gabe-Alonso" target="_blank"><FaGithub size={24}/></a>
+                    </nav>
+                </header>
+                <nav className="nav">
+                    <Link to="/">Home</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/music">Music</Link>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/blog">Blog</Link>
+                    <Link to="/contact">Contact</Link>
+                </nav>
+            </div>
+        </header>
+    )
 }
 
 export default App
